8a32154739308bd0a06ad3ee1c95ac53e8e48e71
William Imoh <williamimoh@yahoo.com>
2023-03-15 00:26:05 +0100

    feat: installed tailwind, done homepage

---
+++ src/components/Footer.tsx
@@ -0,0 +1,18 @@
+import Link from "next/link";
+import { useState } from "react";
+
+export function Footer() {
+  const [mode, setMode] = useState("auto");
+
+  return (
+    <div>
+      <footer id="footer" className="relative z-50 dark:bg-gray-50">
+        <div className="py-5 flex flex-col justify-center items-center">
+          <p className="text-xs lg:text-sm leading-none text-gray-900">
+            2021 Lyne. All Rights Reserved.
+          </p>
+        </div>
+      </footer>
+    </div>
+  );
+}
