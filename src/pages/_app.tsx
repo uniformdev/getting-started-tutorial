e4bee9061691abebd3e187f00cf1faba78387d4a
William Imoh <williamimoh@yahoo.com>
2023-03-16 00:39:58 +0100

    removed Context

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,20 +1,7 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import "@/components/canvasComponents";
-import { Context, ManifestV2 } from "@uniformdev/context";
-import manifest from "../../manifest.json";
-import { UniformContext } from "@uniformdev/context-react";
-
-// Initialize Uniform Context
-const context = new Context({
-  manifest: manifest as ManifestV2,
-  defaultConsent: true,
-});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return (
-    <UniformContext context={context}>
-      <Component {...pageProps} />
-    </UniformContext>
-  );
+  return <Component {...pageProps} />;
 }
